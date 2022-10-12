d25f017f9c69d8cd6e2196ff27a577675277584a
Diana Olarte <dcoa@live.com>
2022-10-11 19:07:38 -0500

    add firebase database to journal app

---
+++ journal-app/src/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore/lite'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: firebase.apiKey,
+  authDomain: firebase.authDomain,
+  projectId: firebase.projectId,
+  storageBucket: firebase.storageBucket,
+  messagingSenderId: firebase.messagingSenderId,
+  appId: firebase.appId
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirestore(FirebaseApp);
